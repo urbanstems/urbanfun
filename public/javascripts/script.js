5b8d56745b9f85f3468204c37d3bc180012e21c5
Omari Mayers-Walker <omari@fastmail.fm>
2016-01-10 11:35:01 -0500

    added marker script

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,6 +4,9 @@
 // confirm its an integer and only five digits long
 
 $(document).ready(function() {
+
+  // checks input for valid zip
+  // makes API call
   $('#zip').submit(function(event) {
     event.preventDefault();
     var zip = $('input').val();
@@ -28,6 +31,7 @@ $(document).ready(function() {
     };
   });
 
+  // validates input for correct zip code format
   function zipCheck(num) {
     if (num.length === 5 && Number(num)) {
       return num;
@@ -36,6 +40,7 @@ $(document).ready(function() {
     }
   }
 
+  // map
   mapboxgl.accessToken =
     'pk.eyJ1Ijoib21hcmltYXllcnN3YWxrZXIiLCJhIjoiODJlZjMxYjhiYjJmZTkwMDBkZDFhYzM2OTU3NDQxZjMifQ.ZjdJwZ3elIR4Ubp0xNC9yw';
   var map = new mapboxgl.Map({
@@ -47,6 +52,25 @@ $(document).ready(function() {
 
   map.addControl(new mapboxgl.Geocoder());
 
-  /* geocoder.query('21202', showMap); */
+  mapbox.featureLayer({
+    // this feature is in the GeoJSON format: see geojson.org
+    // for the full specification
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      // coordinates here are in longitude, latitude order because
+      // x, y is the standard for GeoJSON and many formats
+      coordinates: [-105.0002, 39.7541]
+    },
+    properties: {
+      title: 'Peregrine Espresso',
+      description: '1718 14th St NW, Washington, DC',
+      // one can customize markers by adding simplestyle properties
+      // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+      'marker-size': 'large',
+      'marker-color': '#BE9A6B',
+      'marker-symbol': 'cafe'
+    }
+  }).addTo(map);
 
 });
